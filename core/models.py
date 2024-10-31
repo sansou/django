b8286e341b5f65e5b4f4c32a8665f81074323da4
Thales Souza <santos.thales89@gmail.com>
2024-10-30 22:43:16 -0300

    update core models

--- core/models.py
+++ core/models.py
@@ -4,10 +4,17 @@ from django.db import models
 class Video(models.Model):
   title = models.CharField(max_length=100, unique=True)
   description = models.TextField()
-  thumbnail = models.ImageField(upload_to='thumbnails/')
-  video = models.FileField(upload_to='videos/')
+  thumbnail = models.ImageField(upload_to='media/thumbnails/')
+  video = models.FileField(upload_to='media/videos/')
   slug = models.SlugField(max_length=100, unique=True)
   published_at = models.DateTimeField()
   is_published = models.BooleanField(default=False)
   num_likes = models.IntegerField(default=0)
   num_views = models.IntegerField(default=0)
+  
+  class Meta:
+    verbose_name = 'Vídeo'
+    verbose_name_plural = 'Vídeos'
+
+  def __str__(self) -> str:
+    return self.title

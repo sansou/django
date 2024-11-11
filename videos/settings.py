79af8fb08c3ab9adf5c365090a17a550bb0702a6
Thales Souza <santos.thales89@gmail.com>
2024-11-10 22:29:09 -0300

    liberando os videos para serem visualizados nas rotas

--- videos/settings.py
+++ videos/settings.py
@@ -130,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+
+MREDIA_ROOT = BASE_DIR/ 'media/'
